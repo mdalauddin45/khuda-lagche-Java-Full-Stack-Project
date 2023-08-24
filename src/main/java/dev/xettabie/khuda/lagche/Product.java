008563d066717450cd4a97cc4693d988d3c6129e
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-25 02:40:45 +0600

    product data set

--- src/main/java/dev/xettabie/khuda/lagche/Product.java
+++ src/main/java/dev/xettabie/khuda/lagche/Product.java
@@ -1,2 +1,12 @@
-package dev.xettabie.khuda.lagche;public class Product {
+package dev.xettabie.khuda.lagche;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "products")
+public class Product {
+    private ObjectId id;
+    private String name;
+    private String description;
+    private int price;
 }
