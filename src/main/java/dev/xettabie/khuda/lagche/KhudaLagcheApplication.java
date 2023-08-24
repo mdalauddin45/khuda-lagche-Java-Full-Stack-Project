d7126677494dda53c35f87c4a3165922ddabef4d
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-25 02:35:51 +0600

    env and gitignore add

--- src/main/java/dev/xettabie/khuda/lagche/KhudaLagcheApplication.java
+++ src/main/java/dev/xettabie/khuda/lagche/KhudaLagcheApplication.java
@@ -2,18 +2,12 @@ package dev.xettabie.khuda.lagche;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class KhudaLagcheApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KhudaLagcheApplication.class, args);
 	}
-	@GetMapping("/")
-	public String apiRoot(){
-		return "Hello , world";
-	}
+
 }
