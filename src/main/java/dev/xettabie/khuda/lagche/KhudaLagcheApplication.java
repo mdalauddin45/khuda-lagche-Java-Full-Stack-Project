573dd4086bc03d39b9d211bf5054ed7cfbbaa1ac
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-25 01:45:23 +0600

    first commit

---
+++ src/main/java/dev/xettabie/khuda/lagche/KhudaLagcheApplication.java
@@ -0,0 +1,13 @@
+package dev.xettabie.khuda.lagche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhudaLagcheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhudaLagcheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/xettabie/khuda/lagche/KhudaLagcheApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.xettabie.khuda.lagche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhudaLagcheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
